27a28a174b934b88c89627faecde3860def8277e
hotsunny <shs8man@naver.com>
2021-09-02 22:23:16 +0900

    justice balance 1st

--- js/justice.js
+++ js/justice.js
@@ -1,14 +1,64 @@
 
 let openButton = document.querySelector('.first-open');
 let preContainer = document.querySelector('.pre-container');
-
-console.log("t?")
-
-
+let field = document.querySelector('.judge-field');
+let poem = document.querySelector('.poem');
+let paragraph = poem.querySelectorAll("p");
+let right = document.querySelector('.right-balance');
+let left = document.querySelector('.left-balance');
+let soonseo = 0;
+let total = 0;
+let rightValue = 0;
+let leftValue = 0;
+let rightP = document.querySelectorAll('p');
+let leftP = document.querySelectorAll('p');
 function firstOpen(e) {
-	console.log(this;
+	if (e.target.classList.contains('first-open')) {
+		preContainer.classList.add('hidden');
+	}
+};
+
+function addChild(e) {
+	if (soonseo < 23) {
+		if (total < 50 && total > -50) {
+
+			if (e.target.classList.contains('right-balance' &&'right-field')){
+				right.appendChild(paragraph[soonseo]);
+				total -= 10;
+				soonseo += 1;
+				rightValue += 1;
+			};
+
+			if (e.target.classList.contains('left-balance' && 'left-field')) {
+				left.appendChild(paragraph[soonseo]);
+				total += 10;
+				soonseo += 1;
+				leftValue += 1;
+			};
+			right.style.transform = `translate3d(0, ${-total}px, 0)`;
+			left.style.transform = `translate3d(0, ${total}px, 0)`;
+		} else {
+				alert('균형이 깨졌습니다.');
+				if (total <= -50){
+					total += 10;
+					right.removeChild(rightP[rightValue])
+					rightValue -= 1;
+					soonseo -= 1;
+				} else {
+					total -= 10;
+					left.removeChild(leftP[leftValue])
+					leftValue -= 1;
+					soonseo -= 1;
+				}
+				right.style.transform = `translate3d(0, ${-total}px, 0)`;
+				left.style.transform = `translate3d(0, ${total}px, 0)`;
+			}
+			console.log(soonseo,total,rightValue,leftValue);
+			} 
+	else {
+		return false;
+	}
 }
 
-window.addEventListener('click', firstOpen);
-
-
+preContainer.addEventListener('click', firstOpen);
+field.addEventListener('click', addChild);
