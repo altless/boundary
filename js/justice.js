f5a9becc19b7cc76191056b1051332c35bf59d13
hotsun mac <shs8man@naver.com>
2021-09-02 20:02:54 +0900

    aaa

---
+++ js/justice.js
@@ -0,0 +1,14 @@
+
+let openButton = document.querySelector('.first-open');
+let preContainer = document.querySelector('.pre-container');
+
+console.log("t?")
+
+
+function firstOpen(e) {
+	console.log(this;
+}
+
+window.addEventListener('click', firstOpen);
+
+
