a4bf5308f131c080a2be5c529e90a04863034f33
hotsunny <shs8man@naver.com>
2021-09-10 00:33:40 +0900

    success rotate but not perfect

--- js/copycat.js
+++ js/copycat.js
@@ -5,12 +5,12 @@ console.log(spans);
 
 container.addEventListener('click',function(){
     addAni();
-})
+});
 
 function addAni () {
-    // for (let i=0; i < spans.length; i++) {
-    //     spans.classList.remove('animaiting');
-    // }
+    for (let i=0; i < spans.length; i++) {
+        spans[i].classList.remove('animaiting');
+    }
     for (let i=0; i < 5; i++) {
         let num = parseInt(Math.random()*(spans.length));
     spans[num].classList.add('animaiting');
