454713c11a8fcc0ad8503e6e90d49a99f8c5d72d
hotsunny <shs8man@naver.com>
2021-09-10 00:21:03 +0900

    copycat first but bug

---
+++ js/copycat.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('.container');
+const poem = document.querySelectorAll('.container p');
+const spans = container.querySelectorAll('span');
+console.log(spans);
+
+container.addEventListener('click',function(){
+    addAni();
+})
+
+function addAni () {
+    // for (let i=0; i < spans.length; i++) {
+    //     spans.classList.remove('animaiting');
+    // }
+    for (let i=0; i < 5; i++) {
+        let num = parseInt(Math.random()*(spans.length));
+    spans[num].classList.add('animaiting');
+    };
+    console.log(spans.length);
+}
